13d0d61580590703f14d494b581e234cdcb70d26
joshua <josh@videoplaza.com>
2014-01-08 10:45:39 +0100

    section 2.0.6

--- app/src/tutorial_client/behavior.clj
+++ app/src/tutorial_client/behavior.clj
@@ -9,6 +9,17 @@
 (defn swap-transform [_ message]
   (:value message))
 
+(defn total-count [_ nums] (apply + nums))
+
+(defn maximum [old-value nums]
+  (apply max (or old-value 0) nums))
+
+(defn average-count [_ {:keys [total nums]}]
+  (/ total (count nums)))
+
+(defn merge-counters [_ {:keys [me others]}]
+  (assoc others "Me" me))
+
 (defn init-main [_]
   [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
 
@@ -17,9 +28,17 @@
 
 (def example-app
   {:version 2
-   :transform [[:inc  [:my-counter] inc-transform]
-               [:swap [:**]         swap-transform]]
+   :transform [[:inc [:my-counter] inc-transform]
+               [:swap [:**] swap-transform]]
    :effect #{[#{[:my-counter]} publish-counter :single-val]}
+   :derive #{[{[:my-counter] :me [:other-counters] :others} [:counters] merge-counters :map]
+             [#{[:counters :*]} [:total-count] total-count :vals]
+             [#{[:counters :*]} [:max-count] maximum :vals]
+             [{[:counters :*] :nums [:total-count] :total} [:average-count] average-count :map]}
    :emit [{:init init-main}
-          [#{[:my-counter] [:other-counters :*]} (app/default-emitter [:main])]]})
+          [#{[:my-counter]
+             [:other-counters :*]
+             [:total-count]
+             [:max-count]
+             [:average-count]} (app/default-emitter [:main])]]})
 
